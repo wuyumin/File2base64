227c310d00a21678edd7c2105dc8d05dfd8a9540
吴育民 <wuyumin@wuyumin.com>
2017-10-15 00:43:00 +0800

    run php-cs-fixer fix

--- src/File2base64.php
+++ src/File2base64.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * This file is part of the wuyumin/file2base64.
+ * (c) Yumin Wu
+ * github: https://github.com/wuyumin
+ */
+
 namespace File2base64;
 
 class File2base64
@@ -8,16 +14,17 @@ class File2base64
      * @var array
      */
     protected $typeArray = [
-        'mp3'  => 'audio/mp3',
-        'png'  => 'image/png',
-        'jpg'  => 'image/jpeg',
+        'mp3' => 'audio/mp3',
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg',
         'jpeg' => 'image/jpeg',
-        'gif'  => 'image/gif',
-        'ico'  => 'image/x-icon',
+        'gif' => 'image/gif',
+        'ico' => 'image/x-icon',
     ];
 
     /**
      * File2base64 constructor.
+     *
      * @param array $arr
      */
     public function __construct(array $arr = [])
@@ -27,6 +34,7 @@ class File2base64
 
     /**
      * @param $fileName
+     *
      * @return bool|string
      */
     public function getSuffix($fileName)
@@ -35,24 +43,28 @@ class File2base64
             return false;
         }
         $arr = explode('.', $fileName);
+
         return trim(end($arr));
     }
 
     /**
      * @param $fileName
+     *
      * @return bool|string
      */
     public function toBase64($fileName)
     {
         if ($fileName && $this->getSuffix($fileName) && is_file($fileName) && isset($this->typeArray[$this->getSuffix($fileName)])) {
-            return 'data:' . $this->typeArray[$this->getSuffix($fileName)] . ';base64,' . base64_encode(file_get_contents($fileName));
+            return 'data:'.$this->typeArray[$this->getSuffix($fileName)].';base64,'.base64_encode(file_get_contents($fileName));
         }
+
         return false;
     }
 
     /**
      * @param $fileName
      * @param $filePath
+     *
      * @return bool
      */
     public function toFile($fileName, $filePath)
@@ -62,6 +74,7 @@ class File2base64
                 return true;
             }
         }
+
         return false;
     }
 }
